884e07de7f6ef8eb198a9a216a79c69231f7ba75
KastorRico <sandra.pylypiva@gmail.com>
2020-02-02 19:58:35 +0200

    Some changes

--- main.js
+++ main.js
@@ -8,28 +8,40 @@ $(document).ready(function () {
   
 
   // Add new product
-    $(".bl-add").click(function(){
-      if($("#text").val()){
-        var node	=	$(ROW_TEMPLATE);	//Create	new	HTML	node
+  function	addItem(title)	{
+    var node	=	$(ROW_TEMPLATE);	//Create	new	HTML	node
         var node1 = $(ITEM_TEMPLATE);
-        node.find(".bl-product").text($("#text").val());	//Set	product	title
-        node1.find("#item").text($("#text").val());
+        node.find(".bl-product").text(title);	//Set	product	title
+        node1.find("#item").text(title);
 
         lastAddedId++;
         node.attr('id', lastAddedId);
         node1.attr('id', lastAddedId);
-
-        $("#text").val('');
-        $("#text").focus();
-
-        
+     
       
         LIST.append(node);	//Add	to	the	end	of	the	list
         LEAVE.append(node1);
+  }
+  //  Add by click
+  $(".bl-add").click(function(){
+      var title = $("#text").val()
+      if(title){
+        addItem(title);
+        $("#text").val('');
+        $("#text").focus();
     }
-
   } );
-
+  $("#text").keypress(function(e){
+    if(e.keyCode == 13)
+    {
+      var title = $("#text").val()
+      if(title){
+        addItem(title);
+        $("#text").val('');
+        $("#text").focus();
+      }
+    }
+  } );
   // Delete action
   $( "div.bl-list" ).on( "click", ".bl-buttons .bl-cross", function() {
     var node= $(this).parents('.bl-row');
@@ -55,7 +67,7 @@ $(document).ready(function () {
   $( "div.bl-list" ).on( "click", ".missing .bl-buttons .bl-buy", function() {
     var node= $(this).parents('.bl-row');
     var id = node.attr('id');
-    $(".bl-bb").find("#"+id).css("display","none");
+    $(".bl-bb").find("#"+id).remove();
     $(".bl-leave").find("#"+id).css("display","inline-block");
     node.find(".bl-buy").text("Куплено");
     node.removeClass("missing");
@@ -81,6 +93,20 @@ $(document).ready(function () {
       node.find(".bl-label").text(i-1);
     }
     if(i==2) $(this).css({"background-color":"#ee9e9e","box-shadow":"0px 3px #ee9e9e"});
+  });
+
+  // Change name of the product
+  $( "div.bl-list" ).on( "click", ".bl-product", function() {
+    if ($('input', this).length == 0) {
+      var name = $(this).text().trim();
+      $(this).html('<input class="input-name">');
+      $('input', this).val(name).focus().focusout(
+        function() {
+          var name = $(this).val();
+          $(this).parent().text(name);
+        }
+      );
+    }
   });
 
 } );
